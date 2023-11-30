2a07515dde0a9137fa0419d077fc6a1fca23e759
Kalva014 <kalva014@ucr.edu>
2023-11-30 11:32:38 -0800

    Fixed dijkstra's algo so that it doesn't ignore the walls

--- algorithm.py
+++ algorithm.py
@@ -38,20 +38,19 @@ def BFS(graph, start, stop):
 
         graph.update_grid(graph.grid_list)
 
-# Shortest path algorithm from one node to every other node on the graph
 def dijkstras_algorithm(graph, start, stop):
     # Initialize all the nodes to be infinity
     infinity = 9999999
-    shortest_distance = [[infinity] * 20] * 20 
-    predecessor = [[None] * 20] * 20 # Wrong right here
+    shortest_distance = [[infinity] * 20 for _ in range(20)]  # Corrected initialization
+    predecessor = [[None] * 20 for _ in range(20)]  # Corrected initialization
     path = []
     unseen_nodes = []
-    
+
     # Add every node to unseen nodes
     for curr_node_row in graph.grid_list:
         for i in range(len(curr_node_row)):
             unseen_nodes.append(curr_node_row[i])
-    
+
     # Set starting node to have a distance of 0
     shortest_distance[start.row][start.column] = 0
 
@@ -59,29 +58,29 @@ def dijkstras_algorithm(graph, start, stop):
         min_node = None
 
         for curr_node in unseen_nodes:
-            if min_node is None:
+            if min_node is None or shortest_distance[curr_node.row][curr_node.column] < shortest_distance[min_node.row][min_node.column]:
                 min_node = curr_node
-            elif shortest_distance[curr_node.row][curr_node.column] < shortest_distance[min_node.row][min_node.column]:
-                min_node = curr_node
-        
-        for child_node in min_node.neighbors:
-            if child_node.distance + shortest_distance[min_node.row][min_node.column] < shortest_distance[child_node.row][child_node.column]:
-                shortest_distance[child_node.row][child_node.column] = child_node.distance + shortest_distance[min_node.row][min_node.column]
-                predecessor[child_node.row][child_node.column] = min_node # Wrong right here
 
-        #This code will color the nodes we have already explored yellow
+        for child_node in min_node.neighbors:
+            if not child_node.is_wall():
+                if child_node.distance + shortest_distance[min_node.row][min_node.column] < shortest_distance[child_node.row][child_node.column]:
+                    shortest_distance[child_node.row][child_node.column] = child_node.distance + shortest_distance[min_node.row][min_node.column]
+                    predecessor[child_node.row][child_node.column] = min_node
+
         min_node.set_visited()
         graph.update_grid(graph.grid_list)
 
-        unseen_nodes.remove(min_node) # This might be wrong
+        unseen_nodes.remove(min_node)
 
     current_node = stop
     while current_node != start:
         path.insert(0, current_node)
-        current_node = predecessor[current_node.row][current_node.column] # Wrong right here
+        current_node = predecessor[current_node.row][current_node.column]
 
     path.insert(0, start)
 
-    # Show shortest path and color it on the gui
-    for nodes in path:
+    # Show shortest path and color it on the GUI
+    for node in path:
         node.set_actual_path()
+    
+    graph.update_grid(graph.grid_list)
